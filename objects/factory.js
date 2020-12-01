1859975190c9e931be9e4fd59fc94d9949bebd80
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-01 19:56:05 +0800

    Create an object using a Factory Function

---
+++ objects/factory.js
@@ -0,0 +1,20 @@
+// Factory Functions
+
+function createCircle(radius) {
+    return {
+        radius,
+        // line below is equivalent to the line above
+        // radius: radius
+        diameter() {
+            return 2 * this.radius;
+        }
+        // lines below are equivalent to the lines above
+        // diameter: function () {
+        //     return 2 * this.radius;
+        // }
+    };
+}
+
+const circle = createCircle(12);
+console.log(circle.radius);
+console.log(circle.diameter())
