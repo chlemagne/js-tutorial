de3cdc74ad37f76d60f812996dcb554b9c8df7f0
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-13 11:14:47 +0800

    Add object equality methods to Circle object

--- objects/constructor.js
+++ objects/constructor.js
@@ -5,6 +5,12 @@ function Circle(radius) {
     this.diameter = function () {
         return 2 * this.radius;
     }
+    this.isEqualTo = function(other) {
+        return this.radius === other.radius;
+    }
+    this.isCopyOf = function(other) {
+        return this === other;
+    }
 
     // JS automatically calls line below
     // return this;
@@ -34,3 +40,16 @@ const empty = {};
 const empty2 = new Object();
 console.log(empty.constructor);     // [Function: Object]
 console.log(empty2.constructor);    // [Function: Object]
+
+// object equality
+smallCircle1 = new Circle(5);
+smallCircle2 = new Circle(5);
+bigCircle1 = new Circle(10);
+bigCircle2 = bigCircle1;
+
+console.log("Object equality")
+console.log(smallCircle1.isEqualTo(smallCircle2))
+console.log(smallCircle1.isCopyOf(smallCircle2))
+console.log(smallCircle1.isEqualTo(bigCircle1))
+console.log(bigCircle2.isEqualTo(bigCircle1))
+console.log(bigCircle2.isCopyOf(bigCircle1))
