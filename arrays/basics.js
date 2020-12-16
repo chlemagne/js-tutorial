93e67b207efe4950fb80750529d2dfc337eb715f
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-16 20:14:40 +0800

    Empty an array

--- arrays/basics.js
+++ arrays/basics.js
@@ -10,3 +10,11 @@
     | Remove      | arr.shift()         | arr.splice()        | arr.pop()   |
     +-------------+---------------------+---------------------+-------------+
 */
+
+// To empty clear/empty an array, there are four (4) solutions;
+// the best solution is shown below. Others have quirks and others are long/messy.
+numbers = [1, 2, 3, 4, 5];
+
+// clear numbers
+numbers.length = 0;
+console.log(numbers); // []
