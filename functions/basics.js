76346c44549f723b5ae2688469cef8906e02898a
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-22 22:03:07 +0800

    Demonstrate JS hoisting (i.e. moving declarations to the top)

--- functions/basics.js
+++ functions/basics.js
@@ -1,16 +1,22 @@
 // Basic JS Function
+/*
+    Hoisting is JavaScript's default behavior of moving declarations to the top.
+*/
 
 // Function Declaration
+walk(); // allowed
 function walk() {
     console.log('walk');
 }
 
 // Named Function Expressin
+// run(); not allowed
 let run = function run() {
     console.log('run');
 }
 
 // Anonymous Function Expression
+// swim(); not allowed
 let swim = function() {
     console.log('swim');
 }
