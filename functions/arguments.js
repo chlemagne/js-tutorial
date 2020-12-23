5bfa7f21c98c8faf07d419534879921a5385e612
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-23 08:54:33 +0800

    Demonstrate the Rest operator used in function parameters

--- functions/arguments.js
+++ functions/arguments.js
@@ -16,3 +16,12 @@ function sum() {
     return sum;
 }
 sum(1,2,3,4,5);     // 15
+
+function discountedSum(discount, ...prices) {
+    // ``...prices`` is called a Rest operator
+    // It's read as "discount and the rest of the parameters"
+    // Rest operator must be the last parameter
+    const sum = prices.reduce((a, b) => a + b);
+    return sum * (1 - discount);
+}
+discountedSum(0.1, 20, 30, 25);  // 67.5
