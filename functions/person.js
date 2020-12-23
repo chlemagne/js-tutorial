e68931688fd4337f25096e15d2a3546a79ff9aca
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-23 10:57:30 +0800

    Demonstrate throwing an exception and try-catch block

--- functions/person.js
+++ functions/person.js
@@ -7,7 +7,13 @@ const person = {
         return `${person.firstName} ${this.lastName}`;
     },
     set fullName(name) {
+        if (typeof name !== 'string')
+            throw new Error('Value is not a string.');
+
         const parts = name.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name separated by whitespace.')
+
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
@@ -16,3 +22,11 @@ const person = {
 person.fullName;  // 'Charlemagne Hernani'
 person.fullName = 'Charlie Boy';
 person.fullName;  // 'Charlie Boy'
+
+// JS try-catch
+try {
+    person.fullName = null;
+    person.fullName = '';
+} catch (e) {
+    console.log(e);
+}
