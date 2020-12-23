12849912d78ee80f56387efa80992b23faf6142b
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-23 11:17:17 +0800

    Differentiate JS keywords `let` and `var`

--- functions/basics.js
+++ functions/basics.js
@@ -2,6 +2,18 @@
 /*
     Hoisting is JavaScript's default behavior of moving declarations to the top.
 */
+/*
+    ``let`` vs. ``var`` keywords
+    +-------------------+---------------+-----------------+
+    |                   | let           | var             |
+    +===================+===============+=================+
+    | scope             | block-scoped  | function-scoped |
+    +-------------------+---------------+-----------------+
+    | defined globally  | not attached  | attached        |
+    |                   | to window     | to window       |
+    +-------------------+---------------+-----------------+
+    NOTE: avoid using ``var`` keyword
+*/
 
 // Function Declaration
 walk(); // allowed
