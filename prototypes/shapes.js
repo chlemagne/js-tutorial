42f0123d3e04fa9ef7bdac91de7367b2142440a2
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-29 21:17:00 +0800

    Add Circle object that extends Shape object

--- prototypes/shapes.js
+++ prototypes/shapes.js
@@ -4,9 +4,22 @@
 function Shape() {
 }
 
-
 // Shape.prototype members ares ``static`` and ``class``
 // members in Java and Python, respectively.
 Shape.prototype.duplicate = function() {
     console.log(`Duplicating shape...`);
+}
+
+// Circle object
+function Circle(radius) {
+    this.radius = radius;
+}
+
+// Circle object extends Shape object
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+// Add method to Circle.prototype
+Circle.prototype.draw = function() {
+    console.log(`Drawing a circle with radius <${this.radius}>...`);
 }
