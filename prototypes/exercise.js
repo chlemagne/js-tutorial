a3cf63f0085df2b04b8b1496a97dba0932f3a688
Charlemagne Hernani <chlemagne@gmail.com>
2020-12-31 13:37:03 +0800

    Exercise: Polymorphism

--- prototypes/exercise.js
+++ prototypes/exercise.js
@@ -21,6 +21,11 @@ function HtmlSelectElement(items = []) {
         this.items.splice(this.items.indexOf(item), 1);
     };
 
+    this.render = function() {
+        return `<select>${this.items.map(item => `
+    <option>${item}</option>`).join('')}
+</select>`;
+    };
 }
 
 // HtmlSelectElement.prototype = Object.create(HtmlElement.prototype) is not
@@ -29,8 +34,31 @@ function HtmlSelectElement(items = []) {
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
+function HtmlImageElement(src) {
+    this.src = src;
+
+    this.render = function() {
+        return `<img src=${this.src} />`;
+    };
+}
+
+// Extend HtmlImageElement
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
 const e = new HtmlElement();
 const s = new HtmlSelectElement();
+const i = new HtmlImageElement();
 
 console.log(e);
 console.log(s);
+console.log(i);
+
+// Polymorphism exercise
+const elements = [
+    new HtmlSelectElement(['Apple', 'Banana', 'Carrot']),
+    new HtmlImageElement('http://example.com')
+];
+
+for (let element of elements)
+    console.log(element.render());
