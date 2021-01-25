8b42001f2824afcadc9440a97ce4991b82c26808
Charlemagne Hernani <chlemagne@gmail.com>
2021-01-25 15:59:19 +0800

    [TS] Add properties to typescript class

--- typescript/classes.ts
+++ typescript/classes.ts
@@ -1,15 +1,23 @@
 // Typescript Classes
 
 class Point {
-    constructor(private x: number, private y: number, private isMidpoint?: boolean) {
+    constructor(private _x: number, private _y: number, private _isMidpoint?: boolean) {
+    }
+
+    get isMidpoint() {
+        return this._isMidpoint;
+    }
+
+    set isMidpoint(value: boolean) {
+        this._isMidpoint = value;
     }
 
     draw() {
-        console.log(`Drawing (${this.x}, ${this.y})...`);
+        console.log(`Drawing (${this._x}, ${this._y})...`);
     }
 
     getDistance(another: Point) {
-        console.log(`Calculating distance between (${this.x}, ${this.y}) and (${another.x}, ${another.y})...`);
+        console.log(`Calculating distance between (${this._x}, ${this._y}) and (${another._x}, ${another._y})...`);
     }
 }
 
