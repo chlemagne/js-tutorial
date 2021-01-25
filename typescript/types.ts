0588da001b3053bae739a22579b9dd96c465869d
Charlemagne Hernani <chlemagne@gmail.com>
2021-01-25 13:40:24 +0800

    [TS] Define typescript variables with types; define an enum

---
+++ typescript/types.ts
@@ -0,0 +1,15 @@
+// Typescript Types
+
+let price: number;
+let isActive: boolean;
+let firstName: string;
+let myVar: any;
+let prices: number[] = [1, 2, 3];
+
+enum Color {
+    // NOTE: good practice to explicitly define the indices
+    Red = 0,
+    Green = 1,
+    Blue = 2
+};
+let foregroundColor = Color.Green;
